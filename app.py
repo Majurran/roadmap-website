141d0c3052b81c12c2acb9b478817a3d9aec8ad1
Majurran <91605563+Majurran@users.noreply.github.com>
2023-08-13 16:23:25 +1000

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask
+from flask import Blueprint, render_template, request, flash, jsonify, redirect, url_for, current_app
+import plotly
+import plotly.express as px
+import plotly.graph_objs as go
+import pandas as pd
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template('public-dashboard.html',)
